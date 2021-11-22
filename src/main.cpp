37d40a57836c54a2004dfcc52ac8ad2ddf97a4ab
MAGUENDJI Freud <fmaguendji.ir2022@esaip.org>
2021-11-22 18:58:05 +0100

    update shutdown

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,19 @@
 #include <ArduinoJson.h>
 #include <WiFiUdp.h>
 #include <WiFiClient.h>
+#include <WebSocketsClient.h>
+#include <SocketIOclient.h>
+
+#include <Hash.h>
 #define CONNECTED 0
 #define INIT 1
+#define WAITING 3
 #define DISCONNECTED 2
 #define LED_COLORS_COUNT 72
 #define DEFAULT_DELAY_BETWEEN_LEDS_MS 30
 #define DATA_PIN 2
+#define USE_SERIAL Serial
+
 const char *ssid = "MASTER_5GHZ";
 const char *pwd = "jesuislemotdepasse";
 const int port=8082;
@@ -32,17 +39,21 @@ void listenUDPInfo();
 void loopClient();
 IPAddress getBroadcast();
 void sendJson(const JsonObject& json,char* cmd);
-
+void parseCmd(uint8_t * payload, size_t length);
 CRGB leds[LED_COLORS_COUNT];
 void changeLedsColors(int color,int delayBetweenLedsMs);
 void intToRGB(int color,int *rgb);
+void socketIOEvent(socketIOmessageType_t type, uint8_t * payload, size_t length);
+
+SocketIOclient socketIO;
+
+
 
 void setup() {
    // Connect to wifi
    WiFi.begin(ssid, pwd);
    Serial.begin(9600);
 
-  Serial.println("Connecting to wifi");
    //  Wait esp8266 connected to wifi
    while (WiFi.status() != WL_CONNECTED) {
       delay(500);
@@ -52,17 +63,21 @@ void setup() {
    subnet=WiFi.subnetMask();
    udp.begin(port);
    state=DISCONNECTED;
-   FastLED.addLeds<WS2812B, DATA_PIN>(leds, LED_COLORS_COUNT);
+   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, LED_COLORS_COUNT);
+   for(int i=0;i<LED_COLORS_COUNT;i++){
+     leds[i]=CRGB::Black;
+     delay(30);
+     FastLED.show();
+   }
    }
 void loop() {
   if (state==DISCONNECTED)
   {
     listenUDPInfo();
   }
-  else if (state==CONNECTED)
-  {
-    loopClient();
-  }
+  else
+    socketIO.loop();
+  
 }
 void listenUDPInfo(){
   const size_t CAPACITY = JSON_OBJECT_SIZE(1);
@@ -103,40 +118,37 @@ IPAddress getBroadcast(){
   return IPAddress(b); 
 }
 void tryConnectTCP(const IPAddress &ip,const int port){
-  if(client.connect(ip,8083))
-  {
-    state=CONNECTED;
-    DynamicJsonDocument doc(1024);
-    doc["client"] = "ledcontroller";
-    doc["mac"]=WiFi.macAddress();
-    doc["ip"]=WiFi.localIP().toString();
-    doc["led"]=LED_COLORS_COUNT;
-    doc["startAt"]=millis();
-    doc["delayBetweenLedsMs"]=DEFAULT_DELAY_BETWEEN_LEDS_MS;
-    sendJson(doc.as<JsonObject>(),"deviceinformation");
-  }
-  else
-  {
-    //Serial.println("Connection failed");
-  }
-  
-
+   Serial.println("tryConnectTCP");
+   socketIO.begin("192.168.1.37", 8082, "/socket.io/?EIO=4");
+   socketIO.onEvent(socketIOEvent);
+   state=WAITING;
 }
 
 void handleCmd(JsonObject& data){
     if(data.containsKey("cmd"))
     {
       String cmd=data["cmd"];
-      if(cmd.compareTo("changeLedsColors")==0){
+      if(cmd.compareTo("changeallcolor")==0){
       int color=data["color"];
       int delay=data.containsKey("delay")?data["delay"]:DEFAULT_DELAY_BETWEEN_LEDS_MS;
       changeLedsColors(color,delay);
+      }else if(cmd.compareTo("shutalloffcolor")==0){
+        int dely=data.containsKey("delay")?data["delay"]:DEFAULT_DELAY_BETWEEN_LEDS_MS;
+        for(int i=0;i<LED_COLORS_COUNT;i++)
+        {
+          leds[i]=CRGB::Black;
+          delay(dely);
+          FastLED.show();
+
+        }
       }
+
     }
 }
 void changeLedsColors(int color,int delayBetweenLedsMs){
    int rgb[]={0,0,0};
     intToRGB(color,rgb);
+    Serial.println("change all color event");
   for(int i=0;i<LED_COLORS_COUNT;i++)
   {
     leds[i]=CRGB(rgb[0],rgb[1],rgb[2]);
@@ -152,7 +164,20 @@ void intToRGB(int color,int *rgb){
 };
 void sendJson(const JsonObject& json,char* cmd){
       json["cmd"]=cmd;
-      serializeJson(json,client);      
+      DynamicJsonDocument doc(1024);
+      JsonArray array = doc.to<JsonArray>();
+      array.add("deviceinformation");
+      JsonObject param1 = array.createNestedObject();
+      for (JsonPair kv : json) {
+          param1[kv.key()] = kv.value();
+      }
+        // JSON to String (serializion)
+      String output;
+      serializeJson(doc, output);
+      Serial.println(output);
+        // Send event        
+      socketIO.sendEVENT(output);
+
 }
 void loopClient(){
   if(client.connected())
@@ -177,5 +202,61 @@ void loopClient(){
   else
   {
     state=DISCONNECTED;
+  }
+}
+
+void socketIOEvent(socketIOmessageType_t type, uint8_t * payload, size_t length) {
+    DynamicJsonDocument doc(1024);
+  
+    switch(type) {
+        case sIOtype_DISCONNECT:
+            state=DISCONNECTED;
+            USE_SERIAL.printf("[IOc] Disconnected!\n");
+            break;
+        case sIOtype_CONNECT:
+            USE_SERIAL.printf("[IOc] Connected to url: %s\n", payload);
+            // join default namespace (no auto join in Socket.IO V3)
+            socketIO.send(sIOtype_CONNECT, "/");
+            state=CONNECTED;
+            doc["client"] = "ledcontroller";
+            doc["mac"]=WiFi.macAddress();
+            doc["type"]="ledcontrol";
+            doc["ip"]=WiFi.localIP().toString();
+            doc["led"]=LED_COLORS_COUNT;
+            doc["startAt"]=millis();
+            doc["delayBetweenLedsMs"]=DEFAULT_DELAY_BETWEEN_LEDS_MS;
+            sendJson(doc.as<JsonObject>(),"deviceinformation");
+            break;
+        case sIOtype_EVENT:
+            parseCmd(payload,length); 
+            break;
+        case sIOtype_ACK:
+            USE_SERIAL.printf("[IOc] get ack: %u\n", length);
+            hexdump(payload, length);
+            break;
+        case sIOtype_ERROR:
+            USE_SERIAL.printf("[IOc] get error: %u\n", length);
+            hexdump(payload, length);
+            break;
+        case sIOtype_BINARY_EVENT:
+            USE_SERIAL.printf("[IOc] get binary: %u\n", length);
+            hexdump(payload, length);
+            break;
+        case sIOtype_BINARY_ACK:
+            USE_SERIAL.printf("[IOc] get binary ack: %u\n", length);
+            hexdump(payload, length);
+            break;
+    }
+}
+void parseCmd(uint8_t * payload, size_t length){
+  StaticJsonDocument<1024> doc;
+  auto str=String((char*)payload);
+  deserializeJson(doc,str);
+  JsonArray arr = doc.as<JsonArray>();
+  JsonObject object = arr[1];
+  String event=arr[0].as<String>();
+  if(event.compareTo("cmd")==0)
+  {
+    handleCmd(object);
   }
 }
